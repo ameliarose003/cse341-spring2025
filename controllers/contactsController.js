95db81f6479c196751251cc645eb99edab23bcb1
Ami Halsey <cro19030@byui.edu>
2025-05-07 23:48:42 -0600

    edit to contacts

--- controllers/contactsController.js
+++ controllers/contactsController.js
@@ -1,6 +1,16 @@
-const contact = require('../models/contact');
+const Contact = require('../models/contact');
 
 exports.getAllContacts = async (req, res) => {
-    const contact  = await contact.findById(req.params.id);
-    res.json(contact);
+    const contacts = await Contact.find();
+    res.json(contacts);
+  };
+
+
+exports.getContactById = async (req, res) => {
+const contact = await Contact.findById(req.params.id);
+res.json(contact);
 };
+// exports.getAllContacts = async (req, res) => {
+//     const contact  = await contact.findById(req.params.id);
+//     res.json(contact);
+// };
--- routes/contacts.js
+++ routes/contacts.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const contactsController = require('../controllers/contactsController');
 
 router.get('/', contactsController.getAllContacts);
-router.get('/:id', contactsController.getContactsById);
+router.get('/:id', contactsController.getContactById);
 
 module.exports = router;

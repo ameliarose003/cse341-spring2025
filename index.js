a4c432363b36ba0917d9e2a8805f52035a8280f8
Ami Halsey <cro19030@byui.edu>
2025-05-07 23:30:55 -0600

    another index update

--- index.js
+++ index.js
@@ -5,12 +5,13 @@ const PORT = process.env.PORT || 3000;
 // app.get('/', (req, res) => {
 //     res.send('Hello! my name is Ami Halsey.');
 // });
+app.use(express.json());
 
 app.use('/contacts', require('./routes/contacts'));
 
 
-const nameRoute = require('./routes/name');
-app.use('/', nameRoute);
+// const nameRoute = require('./routes/name');
+// app.use('/', nameRoute);
 
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
